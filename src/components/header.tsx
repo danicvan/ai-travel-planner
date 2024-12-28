7efebe6f015f5202768807f628c6d81e8c708f51
Daniel Evangelista <danicvan@hotmail.com>
2024-12-27 22:08:36 -0300

    feat: install and fix some bugs of tailwindcss and dependencies

---
+++ src/components/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header className="p-4 bg-blue-500 text-white flex justify-between">
+            <h1 className="text-x1 font-bold">AI Travel Planner</h1>
+        </header>
+    )
+}
