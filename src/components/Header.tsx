8b2eba016aa4e66866ea6bcfa5cb4a9d1774c373
Daniel Evangelista <danicvan@hotmail.com>
2025-01-01 20:34:47 -0300

    feat: stylize header components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,25 +3,25 @@ import Image from "next/image";
 export default function Header() {
     return (
         <header>
-            <div>
-                <Image
-                    src="https://e7.pngegg.com/pngimages/168/333/png-clipart-trello-logo-business-microsoft-teams-management-argentina-national-football-team-2018-fifa-world-c-blue-text.png"
-                    alt="Trello logo"
-                    width={50}
-                    height={50}
-                />
-            </div>
+            <div className="flex justify-between items-center max-w-7x1 mx-auto">
+                <div>
+                    <Image
+                        src="https://e7.pngegg.com/pngimages/168/333/png-clipart-trello-logo-business-microsoft-teams-management-argentina-national-football-team-2018-fifa-world-c-blue-text.png"
+                        alt="Trello logo"
+                        width={50}
+                        height={50}
+                    />
+                </div>
 
-            <div>
-                <input
-                    type="text"
-                    placeholder="Search..."
-                    className="w-full p-2 rounded-md text-black"
-                />
-            </div>
+                <div>
+                    <input
+                        type="text"
+                        placeholder="Search..."
+                        className="w-full p-2 rounded-md text-black"
+                    />
+                </div>
 
-            <div>
-                <>
+                <div>
                     <Image
                         src="https://e7.pngegg.com/pngimages/168/333/png-clipart-trello-logo-business-microsoft-teams-management-argentina-national-football-team-2018-fifa-world-c-blue-text.png"
                         alt="Profile photo"
@@ -34,7 +34,7 @@ export default function Header() {
                     >
                         Log Out
                     </button>
-                </>
+                </div>
             </div>
         </header>
     );
