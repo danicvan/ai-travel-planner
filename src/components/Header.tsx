7e70c1f0b41c287cfda044fdda93396c214839db
Daniel Evangelista <danicvan@hotmail.com>
2025-01-06 22:18:41 -0300

    feat: create a link when click on trello icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import { useState } from "react";
 import { FaSearch } from "react-icons/fa";
+import Link from "next/link";
 
 export default function Header() {
     const [isProfileOpen, setIsProfileOpen] = useState(false);
@@ -14,15 +15,20 @@ export default function Header() {
     return (
         <header>
             <div className="flex justify-between items-center max-w-7x1 mx-auto p-4">
+                {/* Trello Logo with Navigation to Home */}
                 <div>
-                    <Image
-                        src="https://cdn.worldvectorlogo.com/logos/trello-logo.svg"
-                        alt="Trello logo"
-                        width={100}
-                        height={100}
-                    />
+                    <Link href="/">
+                        <Image
+                            src="https://cdn.worldvectorlogo.com/logos/trello-logo.svg"
+                            alt="Trello logo"
+                            width={100}
+                            height={100}
+                            className="cursor-pointer"
+                        />
+                    </Link>
                 </div>
 
+                {/* Search Bar and Profile Section */}
                 <div className="flex justify-between items-center max-w-7x1 gap-2">
                     <div className="relative">
                         <input
@@ -30,7 +36,7 @@ export default function Header() {
                             placeholder="Search..."
                             className="w-full p-2 pl-10 rounded-md text-black border border-indigo-600"
                         />
-                        <FaSearch 
+                        <FaSearch
                             className="absolute left-2 top-1/2 transform -translate-y-1/2 text-gray-400"
                             size={16}
                         />
@@ -53,6 +59,7 @@ export default function Header() {
                 </div>
             </div>
 
+            {/* Profile Dropdown */}
             {isProfileOpen && (
                 <div className="absolute top-16 right-4 bg-white shadow-lg rounded-lg p-4 w-64">
                     <h2 className="text-lg font-bold mb-2">Profile</h2>
