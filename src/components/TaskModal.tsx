4978136a5fb4c8cfa0f22de1162816e7b2504ee1
Daniel Evangelista <danicvan@hotmail.com>
2025-01-15 08:14:10 -0300

    feat: import dialog component from headlessui library

--- src/components/TaskModal.tsx
+++ src/components/TaskModal.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Dialog } from "@headlessui/react";
 
 interface TaskModalProps {
     task: { id: string; text: string; imageUrl?: string } | null;
