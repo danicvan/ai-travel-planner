da7c957a6c24897982fe2bbff0798f85eaa20014
Daniel Evangelista <danicvan@hotmail.com>
2025-01-23 07:02:54 -0300

    feat: call the chat api with data message to return a funny message

--- src/components/GreetingMessage.tsx
+++ src/components/GreetingMessage.tsx
@@ -1,20 +1,43 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 type TaskCounts = {
     toDo: number;
     inProgress: number;
     done: number;
 };
 
+const fetchData = async (message: string) => {
+    if (!message) {
+        return console.log(`Message is required!`);
+    };
+    
+    const response = await fetch("http://localhost:5000/api/chat", {
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/json',
+        },
+        body: JSON.stringify({ message }),
+    });
+
+    const result = await response.json();
+    console.log(`My response is: ${result}`);
+    return result;
+};
+
 export default function GreetingMessage({ toDo, inProgress, done }: TaskCounts) {
-    const tasksMessage = `Hello, today, you have ${toDo} tasks in To Do, ${inProgress} In Progress, and ${
-        done === 1 ? "one task" : `${done} tasks`
-    } in Done. Have a productive day!`;
+    const [taskMessage, setTaskMessage] = useState<string | null>(null);
+
+    useEffect(() => {
+        const message = `Make a funny daily message for trello with this data: Todo: ${toDo}, In Progress: ${inProgress}, Done: ${ done === 1 ? '1' : done}.`
+        fetchData(message).then(setTaskMessage);
+    }, [toDo, inProgress, done]);
 
     return (
         <div className="flex items-center justify-center">
             <div className="p-6 bg-white text-indigo-800 rounded-lg shadow-sm max-w-lg text-center">
-                <p className="text-lg">{tasksMessage}</p>
+                <p className="text-lg">{taskMessage}</p>
             </div>
         </div>
     );
