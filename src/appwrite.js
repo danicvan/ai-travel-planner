5bbed8e799594fdcdd1369f61b131cf9a32a465f
daniel <danicvan@hotmail.com>
2025-01-28 08:52:30 -0300

    feat: export appwrite add from appwrite database

--- src/appwrite.js
+++ src/appwrite.js
@@ -1,11 +1,11 @@
 import { Client, Account } from "appwrite";
 
-const client = new Client();
+export const client = new Client();
 
 client
     .setEndpoint("https://cloud.appwrite.io/v1")
     .setProject("ai-travel-planner-backend");
 
-const account = new Account(client);
+export const account = new Account(client);
 
-export { client, account };
+export { ID } from "appwrite";
