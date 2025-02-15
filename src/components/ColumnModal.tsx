647c01baccb5d6adf85339b310a0749728b7c54a
daniel <danicvan@hotmail.com>
2025-02-15 08:29:55 -0300

    feat: create column modal function and export it

--- src/components/ColumnModal.tsx
+++ src/components/ColumnModal.tsx
@@ -6,3 +6,19 @@ interface ColumnModalProps {
     onSave: () => void;
 }
 
+const ColumnModal: React.FC<ColumnModalProps> = ({
+    id,
+    onClose,
+    onDelete,
+    onSave
+}) => {
+    console.log(`call ColumModal component`);
+
+    return (
+        <div>
+            <h1>Column Modal component called</h1>
+        </div>
+    )
+}
+
+export default ColumnModal;
