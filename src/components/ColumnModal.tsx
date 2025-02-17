07bfcee694a98e81cd8d76989e099ef221141cf9
daniel <danicvan@hotmail.com>
2025-02-17 08:06:14 -0300

    feat: style the column modal save buttom

--- src/components/ColumnModal.tsx
+++ src/components/ColumnModal.tsx
@@ -40,16 +40,17 @@ const ColumnModal: React.FC<ColumnModalProps> = ({
                     </textarea>
                 </div>
                 <div className="mt-6 flex justify-end gap-4">
-                <button
-                    onClick={() => handleSave()}
-                >
-                    Save
-                </button>
-                <button
-                    onClick={() => onDelete(id)}
-                >
-                    Delete
-                </button>
+                    <button
+                        onClick={() => handleSave()}
+                        className="rounded-md bg-blue-500 py-1.5 px-3 text-sm font-semibold text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
+                    >
+                        Save
+                    </button>
+                    <button
+                        onClick={() => onDelete(id)}
+                    >
+                        Delete
+                    </button>
                 </div>
             </div>
         </div>
