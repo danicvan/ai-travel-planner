40d5b0d5ca0ec8a7a083270728d2ac4be5ed4e86
daniel <danicvan@hotmail.com>
2025-02-17 07:56:48 -0300

    feat: create element on column modal to place all elements in column

--- src/components/ColumnModal.tsx
+++ src/components/ColumnModal.tsx
@@ -24,30 +24,32 @@ const ColumnModal: React.FC<ColumnModalProps> = ({
 
     return (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-white/70 backdrop-blur-sm">
-            <h2 className="text-lg font-semibold text-gray-900">Column Options</h2>
-            <div className="mt-4">
-                <label className="block text-sm font-medium text-gray-700">Title</label>
-                <textarea
-                    rows={1}
-                    placeholder="Description"
-                    onChange={(e) => setNewTitle(e.target.value)}
-                    value={newTitle}
-                    id="columnTile"
-                    name="columnTitle"
-                    className="mt-2 block w-full rounded-lg border-gray-300 bg-gray-50 py-1.5 px-3 text-sm text-gray-700 focus:ring-2 focus:ring-blue-500 focus:outline-none"
+            <div className="">
+                <h2 className="text-lg font-semibold text-gray-900">Column Options</h2>
+                <div className="mt-4">
+                    <label className="block text-sm font-medium text-gray-700">Title</label>
+                    <textarea
+                        rows={1}
+                        placeholder="Description"
+                        onChange={(e) => setNewTitle(e.target.value)}
+                        value={newTitle}
+                        id="columnTile"
+                        name="columnTitle"
+                        className="mt-2 block w-full rounded-lg border-gray-300 bg-gray-50 py-1.5 px-3 text-sm text-gray-700 focus:ring-2 focus:ring-blue-500 focus:outline-none"
+                    >
+                    </textarea>
+                </div>
+                <button
+                    onClick={() => handleSave()}
                 >
-                </textarea>
+                    Save
+                </button>
+                <button
+                    onClick={() => onDelete(id)}
+                >
+                    Delete
+                </button>
             </div>
-            <button
-                onClick={() => handleSave()}
-            >
-                Save
-            </button>
-            <button
-                onClick={() => onDelete(id)}
-            >
-                Delete
-            </button>
         </div>
     )
 }
