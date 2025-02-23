7fcecba8cfc442c21430e615eb39b4d7263f97f7
daniel <danicvan@hotmail.com>
2025-02-23 09:33:46 -0300

    feat: imimprove the header disposition and explore all the width

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -35,7 +35,7 @@ export default function Header({ onSearch }) {
 
     return (
         <header className="bg-gray-400 sticky top-0 z-50">
-            <div className="flex justify-between items-center max-w-7xl mx-auto px-4 py-3">
+            <div className="flex justify-between items-center w-full mx-2 px-4 py-3">
                 {/* Logo */}
                 <Link href="/">
                     <Image
