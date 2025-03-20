d0e89b0ebfad9bbcc1b57da3b6d6d7387709cbda
daniel <danicvan@hotmail.com>
2025-03-20 09:53:29 -0300

    feat: remove and add separator shadcn component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@
 
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { Separator } from "@radix-ui/react-separator";
+import { Separator } from "@/components/ui/separator";
+
 import { CircleUser, Github } from "lucide-react";
 import { useState } from "react";
 
