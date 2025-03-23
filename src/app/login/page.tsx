b678d28dbffce05e1669bd2dccc4dac9d1ace952
daniel <danicvan@hotmail.com>
2025-03-23 12:34:57 -0300

    refactor: move main page to login page to start to create the landing page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,80 @@
-export default function login () {
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { useRouter } from "next/navigation";
+
+import { CircleUser, Github } from "lucide-react";
+import { useState } from "react";
+
+export default function HomePage() {
+    
+    const [login, setLogin] = useState(false);
+    const router = useRouter();
+
+    const handleProviderLogin = (state: boolean) => {
+        setLogin(state);
+        router.push("/login");
+    }
+
     return (
-        <h1>Sign In</h1>
+        <div className="flex items-center justify-center min-h-screen">
+            <Card className="w-full max-w-md mx-auto">
+                <CardHeader className="space-y-1">
+                    <CardTitle className="text-2xl font-bold">Sign in</CardTitle>
+                    <CardDescription>
+                        Sign in yo your account to continue
+                    </CardDescription>
+                </CardHeader>
+            
+                <CardContent className="space-y-4">
+                    <div className="space-y-2">
+                        <Button
+                            variant="outline"
+                            className="w-full"
+                            type="button"
+                            disabled={login}
+                            onClick={() => handleProviderLogin(true)}
+                        >
+                            <Github className="mr-2 h-4 w-4" />
+                            Continue with GitHub
+                        </Button>
+                        <Button
+                            variant="outline"
+                            className="w-full"
+                            type="button"
+                            disabled={login}
+                            onClick={() => handleProviderLogin(true)}
+                        >
+                        <CircleUser className="mr-2 h-4 w-4"/>
+                        Continue with Google
+                        </Button>
+                    </div>
+
+                    <div className="relative">
+                        <div className="absolute inset-0 flex items-center">
+                            <Separator className="w-full"/>
+                        </div>
+                        <div className="relative flex justify-center text-xs uppercase">
+                            <span className="bg-card px-2 text-muted-foreground">
+                                Or continue with
+                            </span>
+                        </div>
+                    </div>
+                </CardContent>
+                <CardFooter>
+                    <p className="text-sm text-muted-foreground text-center w-full">
+                        Don't have an account?{" "}
+                        <a
+                            href="/register"
+                            className="text-primary hover:text-primary/80 font-medium"
+                        >
+                            Create Account
+                        </a>
+                    </p> 
+                </CardFooter>
+            </Card>
+        </div>
     )
 }
