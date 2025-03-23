ed7a684c7eb47f56f6e68e6fdb375f7fb9b55d16
daniel <danicvan@hotmail.com>
2025-03-23 15:07:22 -0300

    feat: create hero landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Navbar from "@/components/Navbar";
 import Features from "@/components/Features";
+import Hero from "@/components/Hero";
 
 export default function LandingPage () {
     return (
         <div>
             <h1>Landing Page</h1>
+            <Hero />
             <Navbar />
             <Features />
         </div>
