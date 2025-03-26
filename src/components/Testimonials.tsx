163a324de17f51ebf8cfe66cb139730e094579e2
daniel <danicvan@hotmail.com>
2025-03-25 22:38:39 -0300

    feat: create testimonial interface

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,5 +1,14 @@
+interface Testimonial {
+    name?: string;
+    role?: string;
+    content?: string;
+    author?: string;
+    avatar?: string;
+    rating?: number;
+}
+
 export default function Testimonials () {
     return (
-        <h1>Testimonials</h1>
+        <h1>Testimonial</h1>
     )
 }
