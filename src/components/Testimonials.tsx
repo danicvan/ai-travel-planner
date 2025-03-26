94a3eb764f49a4f1da055b94b64a54d091c1cd22
daniel <danicvan@hotmail.com>
2025-03-25 22:51:34 -0300

    feat: create the first one testimonial

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -13,6 +13,18 @@ interface TestimonialProps {
     testimonials?: Testimonial[];
 }
 
+const defaultTestimonials =[
+    {
+        name: "Ana Souza",
+        role: "Gerente de Projetos",
+        content: "Essa ferramenta mudou minha forma de gerenciar projetos no Trello! Agora tenho tudo organizado sem esforço.",
+        author: "Ana Souza",
+        avatar: "https://randomuser.me/api/portraits/women/2.jpg",
+        rating: 5
+    },
+    
+]
+
 export default function Testimonials () {
     return (
         <h1>Testimonial</h1>
