99ef8aa13637ebc12f9b621c5de9fbae0247fa43
daniel <danicvan@hotmail.com>
2025-04-02 19:34:48 -0300

    feat: create and export plan interface

--- src/components/PlanSelector.tsx
+++ src/components/PlanSelector.tsx
@@ -1,3 +1,10 @@
+export interface Plan {
+    id: string;
+    name: string;
+    price: number;
+    features: string[];
+}
+
 export default function PlanSelector() {
     return (
         <h1>PlanSelector</h1>
