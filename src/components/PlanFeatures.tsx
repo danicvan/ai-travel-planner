6d3e6d155f2fb2ab4411288d73f5a3f6d1919594
daniel <danicvan@hotmail.com>
2025-04-03 07:41:38 -0300

    feat: create plan features component

--- src/components/PlanFeatures.tsx
+++ src/components/PlanFeatures.tsx
@@ -1,11 +1,22 @@
+import { Check } from "lucide-react";
 import { Plan } from "./PlanSelector";
 
 interface PlanFeaturesProps {
     plan: Plan;
 }
 
-export default function PlanFeatures () {
+export default function PlanFeatures ({ plan }: PlanFeaturesProps) {
     return (
-        <h1>PlanFeatures</h1>
-    )
+        <div>
+            <h3 className="font-medium mb-2">What's included in {plan.name}:</h3>
+            <ul className="space-y-2">
+                {plan.features.map((feature, index) => (
+                    <li>
+                        <Check className="h-4 w-4 text-primary mr-2 mt-1 flex-shrink-0"/>
+                        <span>{feature}</span>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
