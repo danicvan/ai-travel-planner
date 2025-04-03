4e9e5fdf2eb2d559f48cfad594e6646e405977d6
daniel <danicvan@hotmail.com>
2025-04-03 07:47:25 -0300

    feat; create order summary header

--- src/components/OrderSummary.tsx
+++ src/components/OrderSummary.tsx
@@ -5,8 +5,20 @@ interface OrderSummaryProps {
     isNewUser?: boolean;
 }
 
-export default function OrderSummary () {
+export default function OrderSummary ({ plan, isNewUser }: OrderSummaryProps) {
     return (
-        <h1>Order Summary</h1>
+        <div className="pt-4">
+            <div className="bg-muted/50 p-4 rounded-md">
+                <h3 className="font-medium mb-2">Order Summary</h3>
+                <div className="space-y-2">
+                    <div className="flex justify-between">
+                        <span className="text-muted-foreground">{plan.name}</span>
+                        <span>{plan.price}/month</span>
+                    </div>
+
+                    
+                </div>
+            </div>
+        </div>
     )
 }
