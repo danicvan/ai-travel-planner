8d178590df8ee852e4ba366f127dc5cbc88dfbc4
daniel <danicvan@hotmail.com>
2025-04-03 07:44:13 -0300

    feat: create order summary interface

--- src/components/OrderSummary.tsx
+++ src/components/OrderSummary.tsx
@@ -1,3 +1,10 @@
+import { Plan } from "./PlanSelector";
+
+interface OrderSummaryProps {
+    plan: Plan;
+    isNewUser?: boolean;
+}
+
 export default function OrderSummary () {
     return (
         <h1>Order Summary</h1>
