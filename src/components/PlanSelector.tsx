6224c05f4227a9fa6d18049d024798a9db8563be
daniel <danicvan@hotmail.com>
2025-04-02 22:14:19 -0300

    feat: install radio group ui package and create radio group and radio group item element

--- src/components/PlanSelector.tsx
+++ src/components/PlanSelector.tsx
@@ -1,3 +1,6 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
+import { RadioGroup, RadioGroupItem } from "./ui/radio-group";
+
 export interface Plan {
     id: string;
     name: string;
@@ -11,8 +14,26 @@ interface PlanSelectorProps {
     setSelectedPlan: (plan: string) => void;
 }
 
-export default function PlanSelector() {
+export default function PlanSelector({ plans, selectedPlan, setSelectedPlan}: PlanSelectorProps){
     return (
-        <h1>PlanSelector</h1>
-    )
+        <Card>
+            <CardHeader>
+                <CardTitle>
+                    Choose a Plan
+                </CardTitle>
+                <CardDescription>
+                    Select the plan that works for you
+                </CardDescription>
+            </CardHeader>
+            <CardContent>
+                <RadioGroup value={selectedPlan} onValueChange={setSelectedPlan} className="space-y-4">
+                    {plans.map(plan => (
+                        <div key={plan.id} className={`flex items-center space-x-2 rounded-md border p-4 ${selectedPlan === plan.id ? 'border-primary' : 'border-input'}`}>
+                            <RadioGroupItem value ={plan.id} id={plan.id}/>
+                        </div>
+                    ))}
+                </RadioGroup>
+            </CardContent>
+        </Card>
+    );
 }
