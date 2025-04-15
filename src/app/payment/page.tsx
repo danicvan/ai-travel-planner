f9c5970791dc4f47306d9eefe24e2eec1ebe5850
daniel <danicvan@hotmail.com>
2025-04-14 21:34:48 -0300

    fix: remove order summary and plan features to the payment page to resolve issue during deployment

--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -1,6 +1,4 @@
-import OrderSummary from "@/components/OrderSummary";
 import PaymentForm from "@/components/PaymentForm";
-import PlanFeatures from "@/components/PlanFeatures";
 
 export default function PaymentPage () {
     return (
